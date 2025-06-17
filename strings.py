e42079292a52c2ca601d70d13146bc7887ba2b6a
Mahavir717-virda <mahavirce7@gmail.com>
2025-06-17 22:11:34 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,5 +1,5 @@
 #concatanation of string
-name = "Virda" + " " + "Mahavir"
+name = "virda" + " " + "Mahavir"
 print(name)
 
 #length of the string
@@ -10,3 +10,27 @@ print(name[6:10])
 
 #slicing in strings with negative index
 print(name[-7 : -3])
+
+#string functions 
+
+# 1
+print("\nReturn true if strng start or end with the value")
+print(name.endswith("r"))
+print(name.startswith("V"))
+
+# 2
+print("\nthis function returns the first character of the string in uppercase")
+print(name.capitalize())
+
+# 3
+print("\nAFter replacing old value with new value")
+print(name.replace("virda Mahavir","Khushal"))
+
+# 4 
+print("\nReturns index when value appear first time")
+print(name.find("a"))
+print("Return -1 if don`t in string -> Ex. x",name.find("x"))
+
+# 5 
+print("\nReturn the number of character of times it is coming ")
+print(name.count("a"))
