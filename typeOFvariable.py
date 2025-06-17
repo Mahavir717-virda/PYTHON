c1c00b7270164bc8a474bd5ac378226600643c5d
Mahavir717-virda <mahavirce7@gmail.com>
2025-06-17 21:29:35 +0530

    Add files via upload

---
+++ typeOFvariable.py
@@ -0,0 +1,10 @@
+name = "Mahavir"
+age = 18
+CGPA = 9
+
+print("Printing all the things in same line.")
+print(name, age, CGPA)
+print("\nPrinting all the things in different line.")
+print(name)
+print(age)
+print(CGPA)
