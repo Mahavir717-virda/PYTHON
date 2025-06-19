2891e4a839b8a6ead8b33763b7127298df5645da
Mahavir717-virda <mahavirce7@gmail.com>
2025-06-19 11:25:41 +0530

    Add files via upload

---
+++ tupple.py
@@ -0,0 +1,10 @@
+info = (1,2,3,3,3,3,5)
+
+# Print the tupple
+print(info)
+
+# Print element at index
+print("Index of element 3 -> ",info.index(3))
+
+# print the number how many times the element come
+print("Count of element 3 is ->",info.count(3))
